ae2c789c3fa1a8813c237d46426057204fa55b2e
damiankieltyka <damian.kieltyka@icloud.com>
2019-09-24 21:45:59 +0200

    add shop broker func

--- server.js
+++ server.js
@@ -2,6 +2,11 @@ var express = require('express');
 var app = express();
 app.use(express.static('assets'));
 
+app.use('/store', function(req, res, next){
+    console.log('Jestem pośrednikiem przy żądaniu do /store');
+    next();
+});
+
 app.get('/', function (req, res) {
     res.sendFile('/index.html');
 });
@@ -19,4 +24,13 @@ app.get('/userform', function (req, res) {
         last_name: req.query.last_name
     };
     res.json(response);
+});
+
+app.get('/store', function (req, res) {
+    res.send('To jest sklep');
+});
+
+app.listen(3000);
+app.use(function (req, res, next) {
+    res.status(404).send('Wybacz, nie mogliśmy odnaleźć tego, czego żądasz!')
 });
