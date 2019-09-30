7b9a39fdb262357f0e6e9a86e992117ca6430ec1
damiankieltyka <damian.kieltyka@icloud.com>
2019-09-30 21:48:15 +0200

    add pug

--- server.js
+++ server.js
@@ -1,33 +1,23 @@
 var express = require('express');
 var app = express();
-app.use(express.static('assets'));
 
+app.set('view engine', 'pug');
+app.set('views','./views');
 app.use('/store', function(req, res, next){
     console.log('Jestem pośrednikiem przy żądaniu do /store');
     next();
 });
 
 app.get('/', function (req, res) {
-    res.sendFile('/index.html');
-});
-
-var server = app.listen(3000, 'localhost', function() {
-    var host = server.address().address;
-    var port = server.address().port;
-
-    console.log('Przykładowa aplikacja nasłuchuje na http://' + host + ':' + port);
-});
-
-app.get('/userform', function (req, res) {
-    const response = {
-        first_name: req.query.first_name,
-        last_name: req.query.last_name
-    };
-    res.json(response);
+    res.send('Hello world!');
 });
 
 app.get('/store', function (req, res) {
     res.send('To jest sklep');
+});
+
+app.get('/first-template', function(req, res){
+    res.render('first-template');
 });
 
 app.listen(3000);
